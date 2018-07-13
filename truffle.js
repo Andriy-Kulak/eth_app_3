9e207af9fdc4b7e69022c7c1c4b39b409ce2369f
Andriy Kulak <akulak@fuzzproductions.com>
2018-07-13 14:16:44 -0400

    adjust truffle

--- truffle.js
+++ truffle.js
@@ -6,14 +6,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    // rinkeby: {
-    //   provider: function() {
-    //     return new HDWalletProvider(
-    //       'skill october purchase stairs attack culture click cycle atom maid monster dove',
-    //       'https://rinkeby.infura.io/yGU1efDtsMTxmGUSJOva')
-    //   },
-    //   network_id: 3
-    // }
+    }
   }
 };
